21770eb0e1f14fe443fa79b014f6f328b1d771e1
sionkim00 <48657598+sionkim00@users.noreply.github.com>
2022-03-15 14:52:03 +0900

    create song and player component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Player from "./components/Player";
+import Song from "./components/Song";
+
 export default function App() {
   return (
     <div className="App">
       <h1>Music Player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
---
+++ src/components/Song.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Song() {
+  return (
+    <div className="song-container">
+      <h1>Picture</h1>
+      <h1>Song Name</h1>
+      <h1>Artist</h1>
+    </div>
+  );
+}
